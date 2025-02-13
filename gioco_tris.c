7bffe46ad1942979d6aa4e7f9bdb8f069f3405de
MalagutiEnrico <enrico.malaguti@galileiostiglia.edu.it>
2025-02-13 18:27:31 +0100

    commento del codice di gioco_tris.c

--- gioco_tris.c
+++ gioco_tris.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char matrice[7][7] = {
+char matrice[7][7] = {    //creazione della matrice
     {' ', '1', ' ', '3', ' ', '5', ' ',},
     {'1', ' ', '|', ' ', '|', ' ', ' '},
     {' ', '-', '+', '-', '+', '-', ' ',},
@@ -11,14 +11,15 @@ char matrice[7][7] = {
     {' ', ' ', ' ', ' ', ' ', ' ', ' ',},
 };
 
-void stampa_menu(){
+void stampa_menu(){    //funzione che stampa il menù di gioco
+    system("clear");
     printf("----------Gioco del Tris---------\n");
     printf("Inserisci 1 per iniziare una partita\n");
     printf("Inserisci 0 per uscire dal gioco.\n");
     printf("Effettua la scelta: ");
 }
 
-void stampa_griglia(){
+void stampa_griglia(){    //funzione che stampa la griglia del tris
     for(int x=0; x<7; x++){
         for(int y=0; y<7; y++){
             printf("%c   ", matrice[x][y]);
@@ -27,7 +28,7 @@ void stampa_griglia(){
     }
 }
 
-char scegli_simbolo(char *_simbolo){
+char scegli_simbolo(char *_simbolo){    //funzione che permette all'utente di scegliere il simbolo che inizia a giocare
     do{
         printf("Inserisci il simbolo del giocatore che inzia: ");
         scanf(" %c", &*_simbolo);
@@ -37,7 +38,7 @@ char scegli_simbolo(char *_simbolo){
     }while(*_simbolo != 'x' && *_simbolo != 'o');
 }
 
-int controllo_inserimento(int *_x, int *_y){
+int controllo_inserimento(int *_x, int *_y){    //funzione che controlla le coordinate in input da parte dell'utente
     if(*_x==2 || *_y==2 || *_x==4 || *_y==4 || matrice[*_x][*_y]!=' '){
         return 1;
     }
@@ -46,14 +47,14 @@ int controllo_inserimento(int *_x, int *_y){
     }
 }
 
-void scambia_simbolo(char *_simbolo1, char *_simbolo2){
+void scambia_simbolo(char *_simbolo1, char *_simbolo2){    //funzione che scambia i simboli permettendo di alternare i giocatori
     char temp;
     temp = *_simbolo1;
     *_simbolo1 = *_simbolo2;
     *_simbolo2 = temp;
 }
 
-void inserisci_simbolo(char *_simbolo, int *_x, int *_y){
+void inserisci_simbolo(char *_simbolo, int *_x, int *_y){    //funzione che, dopo il controllo tramite la funzione apposita, inserisce il simbolo nella matrice
     do{
         printf("Inserisci la riga in cui posizionare il simbolo: ");
         scanf("%d", &*_x);
@@ -66,7 +67,7 @@ void inserisci_simbolo(char *_simbolo, int *_x, int *_y){
     matrice[*_x][*_y] = *_simbolo;
 }
 
-int controllo_vittoria(){
+int controllo_vittoria(){    //funzione che controlla un'eventuale vittoria dei giocatori
     //controllo diagonale
     if((matrice[1][1]==matrice[3][3] && matrice[3][3]==matrice[5][5] && matrice[1][1]!=' ') || (matrice[1][5]==matrice[3][3] && matrice[3][3]==matrice[5][1] && matrice[1][5]!=' ')){
         return 1;
@@ -84,7 +85,7 @@ int controllo_vittoria(){
     }
 }
 
-void pulisci_matrice(){
+void pulisci_matrice(){    //funzione che pulisce la matrice, preparandola per una nuova partita
     for(int x=0; x<7; x++){
         for(int y=0; y<7; y++){
             if(matrice[x][y] == 'x' || matrice[x][y] == 'o'){
